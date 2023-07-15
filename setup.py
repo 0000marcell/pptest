7ec10e83d98e72f61901badbc248c290e449aa73
____marcell <0000marcell@gmail.com>
2023-07-15 16:10:03 +0000

    v 1.0.2

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from distutils.core import setup
 setup(
     name = 'pptest',
     packages = ['pptest'],
-    version = '1.0.0',  # Ideally should be same as your GitHub release tag varsion
+    version = '1.0.2',  # Ideally should be same as your GitHub release tag varsion
     description = 'pptest',
     author = 'Marcell Cruz',
     author_email = '0000marcell@gmail.com',
-    url = 'https://github.com/0000marcell/pptest/archive/refs/tags/1.0.0.tar.gz',
+    url = 'https://github.com/0000marcell/pptest/archive/refs/tags/1.0.2.tar.gz',
     download_url = 'download link you saved',
     keywords = ['python'],
     classifiers = [],
