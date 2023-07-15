8c68e3a2395547861767e81811d3c786c9b39ee5
____marcell <0000marcell@gmail.com>
2023-07-15 16:06:33 +0000

    adding the module

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'pptest',
+    packages = ['pptest'],
+    version = '1.0.0',  # Ideally should be same as your GitHub release tag varsion
+    description = 'pptest',
+    author = 'Marcell Cruz',
+    author_email = '0000marcell@gmail.com',
+    url = 'https://github.com/0000marcell/pptest/archive/refs/tags/1.0.0.tar.gz',
+    download_url = 'download link you saved',
+    keywords = ['python'],
+    classifiers = [],
+)
